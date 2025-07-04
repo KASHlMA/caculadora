59c1ceec57576499234df68988939c2a1d2f3667
KASHlMA <gustavoanayalopez32@gmail.com>
2025-07-03 21:12:31 -0600

    Agregue Proyecto

---
+++ src/main/java/com/example/caculadora/HelloController.java
@@ -0,0 +1,71 @@
+package com.example.caculadora;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class HelloController{
+    @FXML
+    private Button btnSumar;
+    @FXML
+    private Button btnRestar;
+    @FXML
+    private Button btnDividir;
+    @FXML
+    private Button btnMultiplicar;
+
+    @FXML
+    protected void sumar(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("sumar.fxml"));
+        Parent root= loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Cambioventada");
+        stage.show();
+
+    }
+
+    @FXML
+    protected void restar(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("restar.fxml"));
+        Parent root= loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Cambioventada");
+        stage.show();
+
+    }
+
+    @FXML
+    protected void dividir(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("dividir.fxml"));
+        Parent root= loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Cambioventada");
+        stage.show();
+
+    }
+
+    @FXML
+    protected void multiplicar(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("multiplicar.fxml"));
+        Parent root= loader.load();
+        Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Cambioventada");
+        stage.show();
+
+    }
+}
